6a5e1cf9b9bea495c1713ef66245759afba4ab07
Andrey <pale_passenger@yahoo.com>
2016-10-03 01:37:18 +0500

    cgame.ga-1.0

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-﻿
+﻿$(function(){
+	$(window).scroll(function(){
+		if($(this).scrollTop()>60) {
+			$('#Navbar').addClass('fixed');
+		}
+		else if($(this).scrollTop()<60) {
+			$('#Navbar').removeClass('fixed');
+		}
+	});
+});
+
+$(function(){
+	$(window).scroll(function(){
+		if(($(this).scrollTop()>85) && ($(window).width()<1000)) {
+			$('#Navbar').addClass('fixed');
+		}
+		else if(($(this).scrollTop()<85) && ($(window).width()<1000)) {
+			$('#Navbar').removeClass('fixed');
+		}
+	});
+});
+
+$(function(){
+	$(window).scroll(function(){
+		if(($(this).scrollTop()>60) && ($(window).width()<383)) {
+			$('#Navbar').addClass('fixed');
+		}
+		else if(($(this).scrollTop()<60) && ($(window).width()<383)) {
+			$('#Navbar').removeClass('fixed');
+		}
+	});
+});
+
+$(document).ready(main);
