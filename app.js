0333d508b94a76eff6c4b297afe3431f30b1468d
Andrey <pale_passenger@yahoo.com>
2016-10-04 06:23:19 +0500

    cgame-1.2

--- app.js
+++ app.js
@@ -33,8 +33,11 @@ $(function(){
 
 $(function(){
 	$(window).scroll(function(){
-		if($(window).width()<383) {
+		if(($(this).scrollTop()>60) && ($(window).width()<383)) {
 			$('.dropdown-content').addClass('fixed');
+		}
+		else if($(this).scrollTop()<60) {
+			$('.dropdown-content').removeClass('fixed');
 		}
 		else if($(window).width()>383) {
 			$('.dropdown-content').removeClass('fixed');
