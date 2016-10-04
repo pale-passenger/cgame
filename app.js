1aaf61b5a23234f01ed9941739a845992114b267
Andrey <pale_passenger@yahoo.com>
2016-10-04 06:52:39 +0500

    cgame-1.2

--- app.js
+++ app.js
@@ -36,12 +36,13 @@ $(function(){
 		if(($(this).scrollTop()>60) && ($(window).width()<383)) {
 			$('.dropdown-content').addClass('fixed');
 		}
-		else if($(this).scrollTop()<60) {
-			$('.dropdown-content').removeClass('fixed');
-		}
+
 		else if($(window).width()>383) {
 			$('.dropdown-content').removeClass('fixed');
 		}
+		else if($(this).scrollTop()<60) {
+			$('.dropdown-content').removeClass('fixed');
+		}	
 	});
 });
 
